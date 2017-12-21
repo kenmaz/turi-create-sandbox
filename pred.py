08cf854945f06ef9c308f04574fb37fabac55452
kenmaz <kentaro.matsumae@gmail.com>
2017-12-22 01:23:29 +0900

    fix

--- pred.py
+++ pred.py
@@ -4,12 +4,19 @@ manga = tc.SFrame.read_csv('mangabox/manga_id-title.csv')
 
 print("==== input ====")
 data = tc.SFrame({
-    'uid': ["kenmaz", "kenmaz", "kenmaz"],
-    'manga_id': [203, 237, 56124],
-    'comp_rate': [1.0, 1.0, 0.1]
+    'uid': ["kenmaz", "kenmaz", "kenmaz", "kenmaz"],
+    'manga_id': [203, 237, 56124, 58443],
+    'comp_rate': [1.0, 1.0, 0.1, 0.1]
     })
 print(data.join(manga, on='manga_id').sort('comp_rate', ascending = False))
 
 print("==== recommend ====")
 res = m.recommend(['kenmaz'], new_observation_data=data)
 print(res.join(manga, on='manga_id').sort('rank'))
+
+print(m.recommend(['new_user']).join(manga, on='manga_id').sort('rank'))
+
+print("==== similar ====")
+sim_src = tc.SFrame({'manga_id': [59579]})
+print(sim_src.join(manga, on='manga_id'))
+print(m.get_similar_items(sim_src['manga_id']).join(manga, on={'similar':'manga_id'}).sort('score', ascending = False))
